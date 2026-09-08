e85402d267849bd1116d4e0b87f3fd35c8e7826a
Ali Alafandy <107328467+AliAlafandy@users.noreply.github.com>
2026-09-08 23:24:21 +0300

    Simplify music playback logic in FreeplayState

--- source/FreeplayState.hx
+++ source/FreeplayState.hx
@@ -311,8 +311,7 @@ class FreeplayState extends MusicBeatState
 		#end
 
 		#if PRELOAD_ALL
-		if (FlxG.save.data.freeplayMusic)
-			FlxG.sound.playMusic(Paths.inst(songs[curSelected].songName), 0);
+		FlxG.sound.playMusic(Paths.inst(songs[curSelected].songName), 0);
 		#end
 
 		var bullShit:Int = 0;
