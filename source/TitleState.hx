ae94c74531e5902c95177faddf27dd468ab45ea5
Ali Alafandy <107328467+AliAlafandy@users.noreply.github.com>
2026-09-08 23:59:04 +0300

    Refactor logoBl initialization to avoid redundancy
    Removed duplicate logoBl properties assignment when Main.watermarks is false.

--- source/TitleState.hx
+++ source/TitleState.hx
@@ -174,17 +174,22 @@ class TitleState extends MusicBeatState
 		{
 			logoBl = new FlxSprite(-150, 1500);
 			logoBl.frames = Paths.getSparrowAtlas('KadeEngineLogoBumpin');
+			logoBl.antialiasing = FlxG.save.data.antialiasing;
+			logoBl.animation.addByPrefix('bump', 'logo bumpin', 24, false);
+			logoBl.updateHitbox();
+			// logoBl.screenCenter();
+			// logoBl.color = FlxColor.BLACK;
 		}
 		else
 		{
 			logoBl = new FlxSprite(-150, -100);
 			logoBl.frames = Paths.getSparrowAtlas('logoBumpin');
+			logoBl.antialiasing = FlxG.save.data.antialiasing;
+			logoBl.animation.addByPrefix('bump', 'logo bumpin', 24, false);
+			logoBl.updateHitbox();
+			// logoBl.screenCenter();
+			// logoBl.color = FlxColor.BLACK;
 		}
-		logoBl.antialiasing = FlxG.save.data.antialiasing;
-		logoBl.animation.addByPrefix('bump', 'logo bumpin', 24, false);
-		logoBl.updateHitbox();
-		// logoBl.screenCenter();
-		// logoBl.color = FlxColor.BLACK;
 
 		gfDance = new FlxSprite(FlxG.width * 0.4, FlxG.height * 0.07);
 		gfDance.frames = Paths.getSparrowAtlas('gfDanceTitle');
