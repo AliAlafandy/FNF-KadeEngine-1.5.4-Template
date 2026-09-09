4a82ebff226f7191965f7fcde94699c79222cc3c
Ali Alafandy <107328467+AliAlafandy@users.noreply.github.com>
2026-09-09 21:09:22 +0300

    Fix syntax error in menuItems array declaration

--- source/PauseSubState.hx
+++ source/PauseSubState.hx
@@ -23,7 +23,7 @@ class PauseSubState extends MusicBeatSubstate
 {
 	var grpMenuShit:FlxTypedGroup<Alphabet>;
 
-	var menuItems:Array<String> = ['Resume', 'Restart song', #if mobileC 'Debug menu' #end, 'Exit to menu'];
+	var menuItems:Array<String> = ['Resume', 'Restart song', #if mobileC 'Debug menu', #end 'Exit to menu'];
 	var curSelected:Int = 0;
 
 	var pauseMusic:FlxSound;
