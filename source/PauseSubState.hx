8e71e572dd6c76c383a8acf0245bee042f06c392
Ali Alafandy <107328467+AliAlafandy@users.noreply.github.com>
2026-09-09 18:14:09 +0300

    Add conditional debug menu item for mobile builds

--- source/PauseSubState.hx
+++ source/PauseSubState.hx
@@ -23,7 +23,7 @@ class PauseSubState extends MusicBeatSubstate
 {
 	var grpMenuShit:FlxTypedGroup<Alphabet>;
 
-	var menuItems:Array<String> = ['Resume', 'Restart song', 'Debug menu', 'Exit to menu'];
+	var menuItems:Array<String> = ['Resume', 'Restart song', #if mobileC 'Debug menu' #end, 'Exit to menu'];
 	var curSelected:Int = 0;
 
 	var pauseMusic:FlxSound;
@@ -253,6 +253,8 @@ class PauseSubState extends MusicBeatSubstate
 						PlayState.instance.removedVideo = true;
 					}
 					FlxG.resetState();
+
+				#if mobileC
 				case "Debug menu":
 					if (PlayState.instance.useVideo)
 					{
@@ -272,6 +274,8 @@ class PauseSubState extends MusicBeatSubstate
 						PlayState.luaModchart = null;
 					}
 					#end
+				#end
+
 				case "Exit to menu":
 					if (PlayState.instance.useVideo)
 					{
