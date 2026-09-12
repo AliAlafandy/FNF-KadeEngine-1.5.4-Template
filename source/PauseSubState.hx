91cd374d95da314207d2243062e4035a598393e9
Ali Alafandy <107328467+AliAlafandy@users.noreply.github.com>
2026-09-12 05:20:16 +0300

    Fix indentation in PauseSubState.hx

--- source/PauseSubState.hx
+++ source/PauseSubState.hx
@@ -258,11 +258,11 @@ class PauseSubState extends MusicBeatSubstate
 				case "Debug menu":
 					if (PlayState.instance.useVideo)
 					{
-					GlobalVideo.get().stop();
-					PlayState.instance.remove(PlayState.instance.videoSprite);
-					FlxG.stage.window.onFocusOut.remove(PlayState.instance.focusOut);
-					FlxG.stage.window.onFocusIn.remove(PlayState.instance.focusIn);
-					PlayState.instance.removedVideo = true;
+						GlobalVideo.get().stop();
+						PlayState.instance.remove(PlayState.instance.videoSprite);
+						FlxG.stage.window.onFocusOut.remove(PlayState.instance.focusOut);
+						FlxG.stage.window.onFocusIn.remove(PlayState.instance.focusIn);
+						PlayState.instance.removedVideo = true;
 					}
 					FlxG.switchState(new ChartingState());
 					//FlxG.stage.removeEventListener(KeyboardEvent.KEY_DOWN, handleInput);
